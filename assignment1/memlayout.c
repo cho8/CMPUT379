d3c93103655abd23bce8660c26ab7229bc07f1e7
CristianDum <cdumitre@ualberta.ca>
2016-09-29 14:08:38 -0600

    almost done get_memdiff

--- assignment1/memlayout.c
+++ assignment1/memlayout.c
@@ -90,35 +90,77 @@ int get_mem_layout (struct memregion *regions, unsigned int size) {
 int get_mem_diff (struct memregion *regions, unsigned int howmany,
   struct memregion *thediff, unsigned int diffsize){
   
+  // This function takes the 'old' memregion, the current 'new' region and
+  // returns what you would have to put on top of 'old' to get 'new'
+
   struct memregion *curr_layout;
   get_mem_layout(curr_layout, howmany);
   
-  int diff_counter
+  int diff_counter = 0;
   int c_old = 0;
   int c_curr = 0;
   
   void *oldTo, *oldFrom, *newTo, *newFrom;
   
-  while (counterOld < howmany) {
-      if (diff_count == diffsize) {
-        return diffsize;
+
+
+  while (c_old < howmany) {
+ //     if (diff_counter == diffsize) {   // must return number of changes, even if bigger than diffsize
+ //     return diffsize;
       }
       oldTo = regions[c_old).to;
       oldFrom = regions[c_old].from;
-      newTo = regions[c_new].to;
-      newFrom = regions[c_new].from;
+      newTo = curr_layout[c_new].to;
+      newFrom = curr_layout[c_new].from;
       
       if (oldTo==newTo && oldFrom==newFrom) {
+        if (regions[c_old].mode != curr_layout[c_curr].mode){
+          thediff[diff_counter] = curr_layout;    // If mode has changed, include new region in thediff
+          diff_counter++;
+        }
+        c_old++;
+        c_curr++;
         continue;
       }
-      if (oldTo < newTo && size) {
-      } else if {
-      } else if {
-      } else if {
-      } else if {
+
+      // Depending on the allignment of the regions, we move to the next one
+      // in the array. If either of the from/to pointers coincide, we don't
+      // include the region in the diff
+
+      if (oldFrom == newFrom) {
+        if (oldTo < newTo){
+          thediff[diff_counter] = current_layout[c_new];
+          diff_counter++;
+          c_new++;
+          continue;
+        } else {
+          c_new++;
+          continue;
+        }
       }
-    
-    
+
+      // New region starts at a different address than old region
+      if (oldTo == newTo) {
+        if (oldTo < oldFrom{
+          thediff[diff_counter] = curr_layout[c_new];
+          diff_counter++;
+        }
+        c_old++;
+        c_new++;
+        continue;
+      }
+
+      // The new region doesn't align with any previous one
+
+      thediff[diff_counter] = curr_layout[c_new];
+      diff_counter++;
+
+      if (oldTo < newTo) {        
+        c_old++;
+      } else {
+        thediff[diff_counter] = curr_layout[c_new];  //if new region took up some of old region, include in thediff
+        c_new++;
+      }
   }
   
   return diff_counter;
