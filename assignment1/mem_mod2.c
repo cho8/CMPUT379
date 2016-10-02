e3cb67279a93b9fd2509fa3179fe501b46e96909
cho8 <cho8@ualberta.ca>
2016-10-02 12:02:52 -0600

    some work on mem_mod2

--- assignment1/mem_mod2.c
+++ assignment1/mem_mod2.c
@@ -5,73 +5,87 @@
 #include "memlayout.h"
 
 /* TODO
-	- take care of segfaults from get_mem_layout
+	- verify memory changes are occuring
 */
 
-
+int PAGE_SIZE=0x4000;
 void init_layout(struct memregion *regions, int size){
 
-//  size = get_mem_layout(regions, 1);		//causes segfault
-//  size = get_mem_layout(regions, size);
+
+  size = get_mem_layout(regions, 1);		// why is size passed in, then reassigned immediately?
+  int actualSize = get_mem_layout(regions, size);
+
+  // in the case where the actual size is larger than the number of array entries,
+  int useSize = actualSize;
+  if (actualSize>size) {
+	  useSize = size;
+  }
+
 
   printf("This is the initial layout of the program memory:\n");
 
-  int i = 0;
-  for (i; i<size; i++) {
+  int i;
+  for (i=0; i<useSize; i++) {
     printf("%p - %p %d \n", regions[i].from, regions[i].to, regions[i].mode);
   }
+  printf("Actual size: %d\n", actualSize);
 
 }
 
 
-void change_layout(struct memregion *old_regions, struct memregion *diff){
 
-  unsigned int size_change = 10;
 
-//  unsigned int size_change = get_mem_diff(old_regions, size, diff, 1);		//get number of entries in new mem_region array
-//  size_change = get_mem_diff(old_regions, size, diff, size_change);	//record changes in diff
+void change_layout(struct memregion *old_regions, int size_or, struct memregion *diff){
 
+  printf("Inside change_layout\n");
+
+
+  int size_change = get_mem_diff(old_regions, size_or, diff, 1);	//get number of entries in new mem_region array
+  int actual_size_change = get_mem_diff(old_regions, size_or, diff, size_change);		//record changes in diff
+  int use_size = actual_size_change;
+
+  // in the case where the actual size is larger than the number of array entries,
+  if (actual_size_change>size_change) {
+	  use_size = size_change;
+  }
   printf("The program memory has been altered. These changes have occured: \n");
   int i;
-  for (i=0; i<size_change; i++) {
-    printf("%p - %p %d \n", diff[i].from, diff[i].to, diff[i].mode);
+  for (i=0; i<use_size; i++) {
+    printf("%-10p - %-10p %d \n", diff[i].from, diff[i].to, diff[i].mode);
   }
+  printf("Actual size change: %d", actual_size_change);
 }
 
 
 int main(){
 
+  int size = 30;	//Size of initial mem_region array
+  int * test;
+  struct memregion in_regions[30];
+  struct memregion diff[30];
 
-  int *size = (int *)malloc(sizeof(int));	//Size of initial mem_region array
-  int * test1 = (int*)malloc(sizeof(int));
-  int * test2 = (int*)malloc(sizeof(int));
+  test = (int*)calloc(sizeof(int));
 
-  struct memregion *in_regions;
-  struct memregion *diff;
-
-  *size = 10;	// DELETE BEFORE SUBMITING
-
-  if(test1 == NULL || test2 == NULL){
+  if(test == 0){
     printf("ERROR: Memory not allocated\n");
     return 1;
   }
 
-  init_layout(in_regions, *size);
+  printf("Get layout:\n");
 
+  init_layout(in_regions, size);
+
+  printf("after init\n");
 
   //This is where we actually change memory space
-  test1 = (int *)realloc(test1, 2 * sizeof(int));
+//  test = (int *)realloc(test, 2 * sizeof(int));
+  test = (int*)calloc(sizeof(int));
 
-  if(test1 == NULL || test2 == NULL){
-    printf("ERROR: Faulty memory alteration\n");
-    return 1;
-  }
 
-  change_layout(in_regions, diff);
+  printf("before change\n");
+  change_layout(in_regions, size, diff);
 
-  free(size);
-  free(test1);
-  free(test2);
+  free(test);
 
   return 0;
 }
